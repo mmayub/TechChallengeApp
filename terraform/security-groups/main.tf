8239bf4ef66c60f40cc734f1f7ffc828ec4abec0
mmayub <104074821+mmayub@users.noreply.github.com>
2022-05-01 19:53:29 +1000

    Adding database security group for the app

--- terraform/security-groups/main.tf
+++ terraform/security-groups/main.tf
@@ -53,8 +53,42 @@ resource "aws_security_group" "ecs_tasks" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   tags = {
     Name        = "${var.name}-sg-task-${var.environment}"
     Environment = var.environment
   }
 }
+
+resource "aws_security_group" "db" {
+  name        = "${var.name}-db-${var.environment}"
+  description     = "Access to port 5432 for application server"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "port 5432 postgres"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.ecs_tasks.id]
+  }
+  
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "${var.prefix}-db"
+  }
+}
+
