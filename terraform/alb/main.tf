e20b9a43f6b6d007f273f5f1a994982980f181df
mmayub <104074821+mmayub@users.noreply.github.com>
2022-05-02 03:02:27 +1000

    updating alb terraform code

--- terraform/alb/main.tf
+++ terraform/alb/main.tf
@@ -1,21 +1,32 @@
 resource "aws_lb" "main" {
   name               = "${var.name}-alb-${var.environment}"
   internal           = false
-  load_balancer_type = "application"
-  security_groups    = var.alb_security_groups
+  security_groups    = var.security_groups
   subnets            = var.subnets.*.id
 
   enable_deletion_protection = false
-
+  
   tags = {
     Name        = "${var.name}-alb-${var.environment}"
     Environment = var.environment
   }
 }
 
+# redirect all traffic to target group from ALB
+resource "aws_alb_listener" "http" {
+  load_balancer_arn = aws_lb.main.id
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.main.arn
+    type             = "forward"
+  }
+}
+
 resource "aws_alb_target_group" "main" {
   name        = "${var.name}-tg-${var.environment}"
-  port        = 80
+  port        = 3000
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "ip"
@@ -36,33 +47,11 @@ resource "aws_alb_target_group" "main" {
   }
 }
 
-resource "aws_alb_listener" "http" {
-  load_balancer_arn = aws_lb.main.id
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = aws_alb_target_group.main.id
-    type             = "forward"
-  }
-}
-
-# Redirect traffic to target group
-# resource "aws_alb_listener" "https" {
-#     load_balancer_arn = aws_lb.main.id
-#     port              = 443
-#     protocol          = "HTTPS"
-
-#     ssl_policy        = "ELBSecurityPolicy-2016-08"
-#     certificate_arn   = var.alb_tls_cert_arn
-
-#     default_action {
-#         target_group_arn = aws_alb_target_group.main.id
-#         type             = "forward"
-#     }
-# }
-
 output "aws_alb_target_group_arn" {
   value = aws_alb_target_group.main.arn
 }
 
+output "alb_dns_name" {
+  description = "public accessible url to access application"
+  value = aws_lb.main.dns_name
+}
--- terraform/alb/variables.tf
+++ terraform/alb/variables.tf
@@ -14,13 +14,21 @@ variable "vpc_id" {
   description = "VPC ID"
 }
 
-variable "alb_security_groups" {
+variable "security_groups" {
   description = "Comma separated list of security groups"
 }
 
-# variable "alb_tls_cert_arn" {
+# variable "certificate_arn" {
 #   description = "The ARN of the certificate that the ALB uses for https"
 # }
+
+# variable "domain_name" {
+#   description = "The domain name to access"
+# }
+
+variable "container_port" {
+  description = "The port where the Docker is exposed"
+}
 
 variable "health_check_path" {
   description = "Path to check if the service is healthy, e.g. \"/status\""
