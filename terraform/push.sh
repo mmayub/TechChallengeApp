120d9de12d1a743139fa95d13af4798bb86a014b
mmayub <104074821+mmayub@users.noreply.github.com>
2022-05-03 03:41:53 +1000

    updating push.sh script

--- terraform/push.sh
+++ terraform/push.sh
@@ -1,11 +1,20 @@
-#!/bin/bash -x
-#
+#!/bin/bash
+# 
 # Builds a Docker image and pushes to an AWS ECR repository
+#
+# Invoked by the terraform-aws-ecr-docker-image Terraform module.
+#
+# Usage:
+#
+# # Acquire an AWS session token
+# $ ./push.sh . 123456789012.dkr.ecr.us-west-1.amazonaws.com/hello-world latest
+#
+
 set -e
-source_path="$1" # 1st argument from command line
-repository_url="$2" # 2nd argument from command line
-tag="${3:-latest}" # Checks if 3rd argument exists, if not, use "latest"
-userid=$4
+
+source_path="$1"
+repository_url="$2"
+tag="${3:-latest}"
 
 # splits string using '.' and picks 4th item
 region="$(echo "$repository_url" | cut -d. -f4)"
@@ -13,12 +22,10 @@ region="$(echo "$repository_url" | cut -d. -f4)"
 # splits string using '/' and picks 2nd item
 image_name="$(echo "$repository_url" | cut -d/ -f2)"
 
-# builds docker image
-(cd "$source_path" && DOCKER_BUILDKIT=1 docker build -t "$image_name" .)
+(cd "$source_path" && docker build -t "$image_name" .)
 
-# login to ecr
-aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $userid.dkr.ecr.$region.amazonaws.com
-# aws --region "$region" ecr get-login-password | docker login --username AWS --password $(aws ecr get-login-password --region $region) ${userid}.dkr.ecr.$region.amazonaws.com
+# get token
+aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$repository_url"
 
 # tag image
 docker tag "$image_name" "$repository_url":"$tag"
