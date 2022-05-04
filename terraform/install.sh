03d8c2b62c39e058049cc943fbed7c0c7c36d555
Malik-Ayub <malik@timetableplus.com.au>
2022-05-04 16:28:52 +1000

    script to seed database and start the application

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,3 +1,6 @@
+# this script is used to set up db and run the app
+# this will run as part of instance launch when the instance is first launched in aws
+# install.sh is passed as user_data in launch configuration
 #!/usr/bin/env bash
 
 sudo yum -y install git
